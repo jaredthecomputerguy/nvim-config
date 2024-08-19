8c2d09f597ecdde24f40220b7677cc8853724e8c
Jared Mercer <jaredthebackpacker@gmail.com>
2024-08-19 13:02:07 -0700

    update file search commands + add c# formatter

--- init.lua
+++ init.lua
@@ -395,8 +395,8 @@ require('lazy').setup({
       local builtin = require 'telescope.builtin'
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-      vim.keymap.set('n', '<leader>p', builtin.find_files, { desc = 'Search [P]roject' })
-      vim.keymap.set('n', '<leader>gf', builtin.git_files, { desc = '[S]earch [G]it Files' })
+      vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Search Whole [P]roject [F]iles' })
+      vim.keymap.set('n', '<leader>p', builtin.git_files, { desc = '[S]earch Git Files in [P]roject' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
@@ -623,6 +623,7 @@ require('lazy').setup({
       format_on_save = true,
       formatters_by_ft = {
         ['c'] = { 'clang_format' },
+        ['cs'] = { 'csharpier' },
         ['lua'] = { 'stylua' },
         ['javascript'] = { { 'prettier', 'prettierd' } },
         ['typescript'] = { { 'prettier', 'prettierd' } },
